f5d39ed6779bfd810dd90d727c02abdbef6da052
Cong Le Xuan <cong.le@rikai.technology>
2026-05-26 08:47:03 +0700

    Initial clean main branch

--- rikaiJavaBai1/src/bai1/Main.java
+++
@@ -1,9 +0,0 @@
-package bai1;
-
-public class Main {
-    public  void main(){
-        System.out.println("hello, java");
-        System.out.println("lê xuân công");
-        System.out.println("2023");
-    }
-}
