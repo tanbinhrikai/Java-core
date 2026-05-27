8aa13d44d4a07f01d4ee72f78e63002ec4d1b4cf
nguyendangduyy <nguyendangduy14102003@gmail.com>
2026-05-27 17:23:17 +0700

    update bai tap day 2

--- src/day2/bai16/Main.java
+++ src/day2/bai16/Main.java
@@ -4,14 +4,24 @@ public class Main {
     static void main() {
         int n = 5;
 
-        int mid = n /2;
         for(int i = 0; i < n; i++) {
-            int k = mid + 1;
-            int l = mid - 1;
-            System.out.println("");
-            for(int j = 0; j < n; j++) {
-
+            for(int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
             }
+            for(int k = 0; k < 2 * i + 1; k++)
+                System.out.print("*");
+
+            System.out.println("");
+        }
+
+        for(int i = n - 1; i >= 0; i--) {
+            for(int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for(int k = 0; k < 2 * i + 1; k++)
+                System.out.print("*");
+
+            System.out.println("");
         }
     }
 }
--- src/day2/bai20/Main.java
+++ src/day2/bai20/Main.java
@@ -4,40 +4,30 @@ import java.util.Scanner;
 
 public class Main {
     static void main() {
-        Scanner scanner = new Scanner(scanner.System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Nhập số hàng n: ");
         int n = scanner.nextInt();
 
-        // Bước 1: Khởi tạo và tính toán mảng 2 chiều
         int[][] triangle = new int[n][n];
-
-        for (int i = 0; i < n; i++) {
-            // Phần tử đầu tiên và cuối cùng của hàng luôn là 1
+        for(int i = 0; i < n; i++) {
             triangle[i][0] = 1;
             triangle[i][i] = 1;
 
-            // Tính các phần tử ở giữa dựa vào hàng trước đó
-            for (int k = 1; k < i; k++) {
+            for(int k = 1; k < i; k++) { // mỗi hàng sẽ có i phần tu mà đầu và cuối đã là fix cung gia tri
+                                            // chỉ cần tính khúc giữa
                 triangle[i][k] = triangle[i - 1][k - 1] + triangle[i - 1][k];
             }
         }
 
-        // Bước 2: In tam giác Pascal canh giữa
-        for (int i = 0; i < n; i++) {
-            // In khoảng trắng ở đầu mỗi hàng để tạo hiệu ứng canh giữa
-            // Càng xuống dưới, số khoảng trắng càng ít đi
-            for (int j = 0; j < n - i - 1; j++) {
-                System.out.print("  "); // Bạn có thể chỉnh 2 khoảng trắng thành 1 tùy độ rộng mong muốn
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n - i - 1; j++) {
+                System.out.print("\t");
             }
 
-            // In các giá trị của hàng i
-            for (int k = 0; k <= i; k++) {
-                // In số kèm theo khoảng trắng phía sau để các số không dính vào nhau
-                System.out.print(triangle[i][k] + "   ");
+            for(int k = 0; k <= i; k++) {
+                System.out.printf("%d\t\t", triangle[i][k]);
             }
-
-            // Xuống dòng sau khi in xong một hàng
-            System.out.println();
+            System.out.println("");
         }
 
     }
