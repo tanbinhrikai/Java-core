2ded4f5926cb5511624bf51e68bfd30daa68e9ea
duynguyen <nguyendangduy14102003@gmail.com>
2026-06-02 17:36:49 +0700

    hoan thien bai tap day 3

--- src/day3/bai11/Main.java
+++ src/day3/bai11/Main.java
@@ -1,10 +1,22 @@
 package day3.bai11;
 
 public class Main {
+    static void thapHaNoi(int n, char cot_bat_dau, char cot_trung_gian, char cot_dich) {
 
+        if(n == 1) {
+            System.out.println("Chuyển đĩa 1 từ cọc " + cot_bat_dau + " sang cọc " + cot_dich);
+            return;
+        }
 
+        thapHaNoi(n - 1, cot_bat_dau, cot_dich, cot_trung_gian);
+
+        System.out.println("Chuyển đĩa " + n + " từ cọc " + cot_bat_dau + " sang cọc " + cot_dich);
+
+        thapHaNoi(n - 1, cot_dich, cot_bat_dau, cot_trung_gian);
+
+    }
 
     static void main() {
-
+        thapHaNoi(3, 'A', 'B', 'C');
     }
 }
--- src/day3/bai19/Main.java
+++ src/day3/bai19/Main.java
@@ -4,9 +4,9 @@ public class Main {
     static boolean isSymmetric(int[][] matrix) {
         int row = matrix.length;
         int col = matrix[0].length;
-        for(int i = 0; i < row; i++) {
-            for(int j = 0; j < col; j++) {
-                if(matrix[i][j] != matrix[j][i]) {
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (matrix[i][j] != matrix[j][i]) {
                     return false;
                 }
             }
@@ -14,15 +14,42 @@ public class Main {
         return true;
     }
 
-    static boolean isIdentity() {
-        return false;
+    static boolean isIdentity(int[][] matrix) {
+        if(isDiagonal(matrix)) {
+            return false;
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (i == j && matrix[i][j] != 1) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 
-    static boolean isDiagonal() {
-        return false;
+    static boolean isDiagonal(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (i != j && matrix[i][j] != 0) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 
-    static void transpose() {
+    static void transpose(int[][] matrix) {
+        int[][] newMatrix = new int[matrix[0].length][matrix.length];
+
+        for(int i =0; i < matrix.length; i++) {
+            for(int j = 0; j < matrix[0].length; j++) {
+                newMatrix[j][i] = matrix[i][j];
+            }
+        }
+        printMatrix(newMatrix);
 
     }
 
@@ -30,9 +57,9 @@ public class Main {
         int row = matrix.length;
         int col = matrix[0].length;
         int sum = 0;
-        for(int i = 0; i < row; i++) {
-            for(int j = 0; j < col; j++) {
-                if(i == j) sum += matrix[i][j];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (i == j) sum += matrix[i][j];
             }
         }
         return sum;
@@ -42,17 +69,24 @@ public class Main {
     static void printMatrix(int[][] matrix) {
         int row = matrix.length;
         int col = matrix[0].length;
-        for(int i = 0; i < row; i++) {
-            for(int j = 0; j < col; j++) {
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
                 System.out.printf("%d\t", matrix[i][j]);
             }
             System.out.println("");
         }
     }
+
     static void main() {
-        int[][] matrix = {{1,2,3}, {2,5,4}, {3,4,6}};
+//        int[][] matrix = {{1, 2, 3}, {2, 5, 4}, {3, 4, 6}};
+//        int[][] matrix = {{1, 0, 0}, {0, 1, 0}, {0, 0, 1}};
+        int[][] matrix = {{1,2,3}, {4,5,6}};
+
         printMatrix(matrix);
         System.out.printf("Tong duong cheo chinh: %d %n", trace(matrix));
         System.out.printf("La ma tran doi xung: %b %n", isSymmetric(matrix));
+        System.out.printf("La ma tran duong cheo: %b %n", isDiagonal(matrix));
+        System.out.printf("La ma tran don vi: %b %n", isIdentity(matrix));
+        transpose(matrix);
     }
 }
